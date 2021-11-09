453b356cc797bcf29ff4716ae64ad3a61fa21f0a
Nathan Chow <emailme@nathanchow.ca>
2021-11-08 21:18:50 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import logo from './logo.svg';
 import React, { useState } from 'react';
-import { ApolloProvider } from '@apollo/react-hooks';
-import ApolloClient from 'apollo-boost';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Auth from './utils/auth';
 import { UserProvider } from './utils/GlobalState';
@@ -13,24 +11,10 @@ import TopDisplay from './components/TopDisplay/TopDisplay';
 import './App.css';
 import './css/standard.css';
 
-// const client = new ApolloClient({
-//   request: operation => {
-//       const token = localStorage.getItem('id_token');
-//       operation.setContext({
-//           headers: {
-//               authorization: token ? `Bearer ${token}` : ''
-//           }
-//       });
-//   },
-//   uri: '/graphql'
-// });
-
 function App() {
 
   return (
-      // <ApolloProvider client={client}>
         <Router>
-          <UserProvider>
             <div className="display-flex-column nuxe-colour page-font">
               <TopDisplay />
               <Header 
@@ -46,9 +30,7 @@ function App() {
               <Footer 
               />
             </div>
-          </UserProvider> 
         </Router>
-      // </ApolloProvider>
     );
 }
 export default App;
