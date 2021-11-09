0152fc751260beed8dd2b25da5b483aec487b796
Nathan Chow <emailme@nathanchow.ca>
2021-11-08 21:15:14 -0500

    update

--- server/models/index.js
+++
@@ -1,6 +0,0 @@
-const User = require('./User');
-const Promo = require('./Promo');
-const Order = require('./Order');
-const Product = require('./Product');
-
-module.exports = { User, Promo, Order, Product };
