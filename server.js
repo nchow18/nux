8f057563ceb698505f6f63522765968389f5c0cc
Nathan Chow <emailme@nathanchow.ca>
2021-12-07 23:41:16 -0500

    update

--- server.js
+++ server.js
@@ -25,13 +25,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true }));
 // app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, './build')));
 
 //turn on routes
 app.use(routes);
 
-app.get("*", (req, res) => {
-  let url = path.join(__dirname, '../build', 'index.html');
-  res.sendFile(url);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './build/index.html'));
 });
 
 // turn on connection to db and server
