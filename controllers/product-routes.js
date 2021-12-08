6278d3c0cbeec435d13767cafd52ab0b48ba5f3b
Nathan Chow <emailme@nathanchow.ca>
2021-12-07 23:48:45 -0500

    update

---
+++ controllers/product-routes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { User, Product } = require('../models');
+
+router.get('/', (req, res) => {
+  console.log(req.session);
+  Product.findAll({
+    attributes: [
+      'id',
+      'category',
+      'details',
+      'colors'
+    ]
+  })
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
+})
+
+module.exports = router;
