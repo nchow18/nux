3c395e2ddad0e46488492fe0458744730617923c
Nathan Chow <emailme@nathanchow.ca>
2021-12-07 22:52:49 -0500

    update

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -28,6 +28,29 @@ function Product(props) {
   const categoryURL = window.location.href.split('/products/')
   const category = categoryURL[1]
 
+  const lengths = [
+    {
+      name: 'length_18',
+      length: '18"'
+    },
+    {
+      name: 'length_20',
+      length: '20"'
+    },
+    {
+      name: 'length_22',
+      length: '22"'
+    },
+    {
+      name: 'length_24',
+      length: '24"'
+    },
+    {
+      name: 'length_26',
+      length: '26"'
+    }
+  ]
+
   useEffect(() => {
 
     setCat()
@@ -79,12 +102,20 @@ function Product(props) {
 
   }
 
-  console.log(isColorArr);
-  console.log(isPrice);
-  console.log(productArr);
+  function updateLengthPrice(len) {
 
-  function updateLengthPrice() {
+    const color = colorRemoveCode(isColor);
 
+    if (category === 'classic_tape_in') {
+      setColorArr(productArr[0][color])
+      setPrice(productArr[0][color][len]);
+    } else if (category === 'invisible_tape_in') {
+      setColorArr(productArr[1][color])
+      setPrice(productArr[1][color][len]);
+    } else if (category === 'seamless_tape_in') {
+      setColorArr(productArr[2][color])
+      setPrice(productArr[2][color][len]);
+    }
   }
 
   function addQty() {
@@ -103,6 +134,15 @@ function Product(props) {
     }
   }
 
+  function formatLength(code) {
+
+    const new_length = code.split('_')
+
+    console.log(new_length[1])
+
+    return new_length[1];
+  }
+
   function addCart(amount) {
 
     var updateCart = [];
@@ -110,7 +150,8 @@ function Product(props) {
       category: category.replaceAll('_', ' ').toUpperCase(),
       price: (isColorArr[isLength] * amount),
       color: colorFullName(isColor),
-      quantity: amount
+      quantity: amount,
+      length: `${formatLength(isLength)}"`
     }
 
     updateCart = [...isCart, addToCart]
@@ -191,7 +232,7 @@ function Product(props) {
             {isColoursModal === true && (
               <div className="product-menu-modal-contents">
                {colors.map((col) => (
-                    <div className="product-picture-mini-container">
+                    <div className={`product-picture-mini-container ${isColor === col && `colour-active`}`}>
                       <img onClick={() => {setColor(col); sortColor(col)}} className="product-picture-mini" key={col} alt={col} src={process.env.PUBLIC_URL + `/images/Products/Colors/${col.replace('(', '').replace(')', '')}.png`} />
                     </div>
                ))}
@@ -200,12 +241,20 @@ function Product(props) {
           </div>
           <div className="product-menu-description-container">
             <div onClick={() => {setDesModal(false); setColoursModal(false); setCustModal(false); setLengthModal(true)}} className="product-menu-description-title">
-              <p>LENGTH</p>
+              <p>LENGTH (INCH)</p>
               <img alt="downarrow" src={process.env.PUBLIC_URL + `/images/Products/Icons/down-arrow-light.png`} />
             </div>
             {isLengthModal === true && (
               <div className="product-menu-modal-contents">
-                LEN
+                {lengths.map((len) => (
+                  <div onClick={() => {setLength(len.name); updateLengthPrice(len.name)}} className={`product-length-option ${isLength === len.name && `length-active`}`}>
+                    <div>{len.length}</div>
+                  </div>
+                ))}
+                <div className="product-hair-length-title">Visual Hair Length Estimation</div>
+                <div className="product-length-picture">
+                  <img alt="hair_image" className="product-hair-length-pic" src={process.env.PUBLIC_URL + `/images/Products/Icons/${isLength}.png`} />
+                </div>
               </div>
             )}
           </div>
@@ -216,7 +265,15 @@ function Product(props) {
             </div>
             {isCustModal === true && (
               <div className="product-menu-modal-contents">
-                CUS
+                <div className="product-customize-contents">
+                  <p>We can customize all orders in any weight 9100g and over only), width, length and the colour of the extension you prefer.  If you don't see the extension, ask us and we will amke the custom colour for you.</p>
+                  <p>You can reach out to us at</p>
+                  <p>
+                    <a href="">IG: @nuxe.extensionbar</a><br></br>
+                    <a href="mailto: support@nuxehair.com">Email: support@nuxehair.com</a><br></br>
+                    or click <u><a href="">here</a></u> to our Contact Page
+                  </p>
+                </div>
               </div>
             )}
           </div>
