cc4bb3ab1d1d38f90f47a796d18670aa3cab4ade
Nathan Chow <emailme@nathanchow.ca>
2022-03-24 16:03:40 -0400

    update

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,8 +4,5 @@ const productRoutes = require('./product-routes.js');
 
 router.use('/users', userRoutes);
 router.use('/products', productRoutes);
-router.use('/products/seamless_tape_in', productRoutes);
-router.use('/products/classic_tape_in', productRoutes);
-router.use('/products/invisible_tape_in', productRoutes);
 
 module.exports = router;
